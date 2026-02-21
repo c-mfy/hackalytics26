e21b4e585d1481a3d38f177fbe8e88f22ef14e30
c-mfy <xinyusu06@gmail.com>
2026-02-21 01:31:42 -0500

    App Backend Start

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,3 +6,16 @@
 #   code to read CSV and compute data
 
 
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Serve static files from the "static" folder
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Serve index.html at the root
+@app.get("/")
+async def home():
+    return FileResponse("static/index.html")
